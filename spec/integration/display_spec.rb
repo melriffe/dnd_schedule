f35905625a5a93ddcb12a93f900f70944e85abc7
Mel Riffe <mel@juicyparts.com>
2020-09-11 15:25:47 -0400

    Sort out some tests
    There is still 1 failing test. I will fix that soon-ish.

--- spec/integration/display_spec.rb
+++ spec/integration/display_spec.rb
@@ -3,12 +3,27 @@ RSpec.describe "`dnd_schedule display` command", type: :cli do
     output = `dnd_schedule help display`
     expected_output = <<-OUT
 Usage:
-  dnd_schedule display
+  dnd_schedule display [OPTIONS]
 
 Options:
-  -h, [--help], [--no-help]  # Display usage information
+  -a, [--all], [--no-all]            # Display schedule for all games
+  -u, [--upcoming], [--no-upcoming]  # Display schedule for games in the next 2 weeks
+                                     # Default: true
+  -l, [--list], [--no-list]          # Display a list of configured games
+  -g, [--game=GAME]                  # Display schedule for specified game
+  -e, [--exclude=GAME]               # Exclude schedule for specified game
+  -m, [--month=N]                    # Display schedule for specified month
+  -h, [--help], [--no-help]          # Display usage information
+      [--debug], [--no-debug]        # Run in debug mode
 
-Display D&D Schedule
+Description:
+  Display D&D Schedule(s). Based on information stored in 'config.yml' this command displays the various D&D games and their scheduled dates.
+
+  The default behavior is to display the games scheduled in the next 2 weeks; the --upcoming flag.
+
+  You can also specify specific games, or specific months with which to display schedules.
+
+  The --all command overrides all passed in options.
     OUT
 
     expect(output).to eq(expected_output)
